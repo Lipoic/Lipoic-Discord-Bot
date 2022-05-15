7fcdfa43b8056e781c24d8133414ad7be98c608b
watermelon1024 <st102060@chps.tp.edu.tw>
2022-05-15 17:32:43 +0800

    exp/hello-command
    Add Hello slash command.
    Add Ping slash command.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Lipoic-Discord-Bot
 
 the bot of Lipoic official discord server
+
+```sh
+python -m venv env
+env\Scripts\activate
+pip install py-cord==2.0.0b7
+```
---
+++ main.py
@@ -0,0 +1,18 @@
+import discord , tokens
+
+# prefix: str = "!"
+# intents = discord.Intents.default()
+
+bot: discord.Bot = discord.Bot()
+
+@bot.event
+async def on_ready():
+  print("Log in as " + str(bot.user))
+
+for cog in [
+  "hello",
+  "clear"
+  ]:
+  bot.load_extension(cog)
+
+bot.run(tokens.bot)
