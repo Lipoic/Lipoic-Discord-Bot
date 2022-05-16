dfd98c7172f175926667b84acd89e0884f8cb832
Mango <evan940415@gmail.com>
2022-05-16 21:31:38 +0800

    feat: Dynamic Voice Channel

---
+++ cogs/events/dvoice.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+class DvoiceCog(commands.Cog):
+    def __init__(bot) -> None:
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_voice_stats_update(self, 
+                                    member: discord.Member, 
+                                    before: discord.VoiceState, 
+                                    after: discord.VoiceState):
+        if after.channel.id == 942291362861170699: #! For Test
+            await after.channel.category.create_voice_channel(name=member)
+        
+        if not before.channel.members:
+            await before.channel.delete()
+
+def setup(bot):
+    bot.add_cog(DvoiceCog(bot))
