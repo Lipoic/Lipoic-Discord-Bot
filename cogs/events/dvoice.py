8566bd716bb588e5a9707ae0d99eacd44f70540f
watermelon1024 <st102060@chps.tp.edu.tw>
2022-05-17 18:23:55 +0800

    perf: detect
    Perf "if arg not is None" detect.

--- cogs/events/dvoice.py
+++ cogs/events/dvoice.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 
 class DynamicVoiceCog(commands.Cog):
-    def __init__(self, bot) -> None:
+    def __init__(self, bot):
         self.bot = bot
         self.dvc_dict = {}
 
@@ -12,14 +12,14 @@ class DynamicVoiceCog(commands.Cog):
                                     member: discord.Member, 
                                     before: discord.VoiceState, 
                                     after: discord.VoiceState):
-        if not after.channel is None and after.channel.id == 975755760032165888: # Channel ID Just For Test
+        if after.channel and after.channel.id == 975755760032165888: # Channel ID Just For Test
             try:
                 dvc = self.dvc_dict[str(member.id)]
             except:
                 dvc = await after.channel.category.create_voice_channel(name=f"{member.display_name}的頻道")
                 self.dvc_dict[str(member.id)] = dvc
             await member.move_to(dvc)
-        if not before.channel is None and not before.channel.members and before.channel in list(self.dvc_dict.values()):
+        if before.channel and not before.channel.members and before.channel in list(self.dvc_dict.values()):
             await before.channel.delete()
             for i in self.dvc_dict:
                 if self.dvc_dict[i] == before.channel:
