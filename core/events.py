e03fdcb0540327435ffffa0d6baf7473123ed49e
猴子 <70706886+a3510377@users.noreply.github.com>
2022-05-18 16:40:10 +0800

    Update events.py

--- core/events.py
+++ core/events.py
@@ -10,12 +10,12 @@ if TYPE_CHECKING:
     from core import LIPOIC
 
 
-class MainEventsCog(commands.Cog):
+class MainEventsCog(discord.Cog):
     def __init__(self, bot: 'LIPOIC') -> None:
         self.bot = bot
         self.log = bot.log
 
-    @commands.Cog.listener()
+    @discord.Cog.listener()
     async def on_ready(self):
         bot = self.bot
         if bot._uptime is not None:
@@ -25,8 +25,10 @@ class MainEventsCog(commands.Cog):
         bot.log.info(bot.user)
         bot._is_ready.set()
 
-    @commands.Cog.listener()
+    @discord.Cog.listener()
     async def on_application_command_error(self, ctx: discord.ApplicationContext, error: discord.DiscordException):
         print(type(error))
         if isinstance(error, discord.ApplicationCommandInvokeError):
             await ctx.respond("此命令不能在私人消息中使用")
+        elif isinstance(error, commands.MissingPermissions):
+            await ctx.respond("你沒有權限使用此命令")
