a35a20cd96287f6aa4966f7beb02d42ea09520e8
猴子 <70706886+a3510377@users.noreply.github.com>
2022-06-10 10:05:07 +0800

    feat: add event class once event

--- utils/Events.py
+++ utils/Events.py
@@ -2,23 +2,28 @@ import asyncio
 from typing import Callable, Coroutine, Any, Dict, List, Optional
 
 
+__all__ = ('Events')
+
 CoroFunc = Callable[..., Any]
 
 
 class Events:
     def __init__(self):
         self.extra_events: Dict[str, List[CoroFunc]] = {}
-        self._prependEvent = {}
 
-    def addEventListener(self, func: CoroFunc, name: Optional[str] = None) -> None:
+    def add_event_listener(self, func: CoroFunc, name: Optional[str] = None, /, once: bool = False) -> None:
         name = name or func.__name__
+
+        if once:
+            self.once(func, name)
+            return
 
         if name in self.extra_events:
             self.extra_events[name].append(func)
         else:
             self.extra_events[name] = [func]
 
-    def removeEventListener(self, func: CoroFunc, name: Optional[str] = None) -> None:
+    def remove_event_listener(self, func: CoroFunc, name: Optional[str] = None) -> None:
         name = name or func.__name__
 
         if name in self.extra_events:
@@ -27,26 +32,17 @@ class Events:
             except ValueError:
                 ...
 
+    def once(self, func: CoroFunc, name: Optional[str] = None) -> None:
+        self.add_event_listener(
+            lambda: self.remove_event_listener(func, name), name
+        )
+        self.add_event_listener(func, name)
+
     def on(self, func: CoroFunc, name: Optional[str] = None) -> None:
-        return self.addEventListener(func, name)
+        self.add_event_listener(func, name)
 
-    def prependListener(self, event) -> None:
-        ...
-
-    def emit(self, event) -> None:
-        ...
-
-    @property
-    def events_names(self) -> List[str]:
-        return self.extra_events.keys()
-
-    @property
-    def listener_count(self) -> int:
-        return len(self.extra_events.keys())
-
-    @property
-    def listeners(self, eventName: str) -> List[CoroFunc]:
-        return self.extra_events.get(eventName, [])
+    def emit(self, event_name: str, *args: Any, **kwargs: Any) -> None:
+        self.dispatch(event_name, *args, **kwargs)
 
     def dispatch(self, event_name: str, *args: Any, **kwargs: Any) -> None:
         event_name = f'on_{event_name}'
@@ -61,10 +57,11 @@ class Events:
         **kwargs: Any,
     ) -> None:
         if asyncio.iscoroutinefunction(coro):
-            return asyncio.create_task(
+            asyncio.create_task(
                 self._run_async_event(coro, event_name, *args, **kwargs),
                 name=f'{self.__class__.__name__}: {event_name}'
             )
+            return
         return self._run_event(coro, event_name, *args, **kwargs)
 
     async def _run_async_event(
@@ -94,3 +91,15 @@ class Events:
         except Exception as er:
             if event_name != 'error':
                 self.dispatch('error', *args, **kwargs, error=er)
+
+    @property
+    def events_names(self) -> List[str]:
+        return self.extra_events.keys()
+
+    @property
+    def listener_count(self) -> int:
+        return len(self.extra_events.keys())
+
+    @property
+    def listeners(self, eventName: str) -> List[CoroFunc]:
+        return self.extra_events.get(eventName, [])
